489222b06e801b8fb3a6a0fc8c76bddd8b72c108
jtj3107 <jtj3926@gmail.com>
2022-06-05 18:55:26 +0900

    Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,41 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
-export default function Home({results}) {
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push({
+      pathname: `/movies/${id}`,
+      query: {
+        title,
+      },
+    }, 
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
-    <Seo title="Home" />
-    {results?.map((movie) => (
-      <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>
-    ))}
-    <style jsx>{`
+      <Seo title="Home" />
+      {results?.map((movie) => (
+        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+          <Link 
+            href={{
+              pathname: `/movies/${movie.id}`,
+              query: {
+                title: movie.original_title,
+              },  
+            }}
+            as={`/movies/${movie.id}`}
+            >
+            <a>{movie.original_title}</a>
+          </Link>
+          </h4>
+        </div>
+      ))}
+      <style jsx>{`
         .container {
           display: grid;
           grid-template-columns: 1fr 1fr;
@@ -36,10 +60,10 @@ export default function Home({results}) {
         }
       `}</style>
     </div>
-   );
+  );
 }
 
-export async function getServerSideProps(){
+export async function getServerSideProps() {
   const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
   return {
     props: {
